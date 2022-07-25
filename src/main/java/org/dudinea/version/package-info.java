181f633842e58228adccb0e94496df9b04b5842d
Eugene Doudine <eugene.d@trekflow.com>
2022-07-25 17:09:36 +0300

    docs: add package-info

---
+++ src/main/java/org/dudinea/version/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Version is an immutable that reporesents software version.
+ * It supports the specifications of Semantic Versioning and can
+ * be used like a Number (arithmetic operations and comparisons are supported).
+ * 
+ */
+package org.dudinea.version;
+
